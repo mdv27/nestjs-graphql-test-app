3fa1daaa40b5e364ee0cf7bbd6b0afde20b17b6e
Madhusudan Vishnupurikar <madhusudan.vishnupurikar@bp.com>
2022-08-01 14:09:17 +0530

    nestjs app.

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CatsModule } from './src/cats/cats.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    CatsModule,
+    ServeStaticModule.forRoot({
+        rootPath: join(__dirname, '..', 'client')
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+        driver: ApolloDriver,
+        typePaths: ['./**/*.graphql'],            
+        installSubscriptionHandlers: true
+    })
+  ]
+})
+export class AppModule {}
